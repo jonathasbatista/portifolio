97cb2ce5a41a683c82fb3466a8600326c4dc8fb8
Jonathas <jonathasgon99@gmail.com>
2025-04-07 16:55:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navLink.classList.toggle('active');
+}
